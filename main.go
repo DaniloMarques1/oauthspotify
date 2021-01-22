e3f767e3308a0c5ce8614c5d02743981fa1cd1df
Danilo Marques <danilomarques20@hotmail.com>
2021-01-22 18:50:16 -0300

    Displaying information about the first 10 last played tracks

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -39,6 +40,30 @@ type TokenResponse struct {
 	Scope         string `json:"scope"`
 	Expires_in    int64  `json:"expires_in"`
 	Refresh_token string `json:"refresh_token"`
+}
+
+// recently played tracks response
+type Response struct {
+	Items []Item `json:"items"`
+}
+
+type Item struct {
+	Track     Track  `json:"track"`
+	Played_at string `json:"played_at"`
+}
+
+type Track struct {
+	Album Album  `json:"album"`
+	Name  string `json:"name"`
+}
+
+type Album struct {
+	Artists []Artist `json:"artists"`
+	Name    string   `json:"name"`
+}
+
+type Artist struct {
+	Name string `json:"name"`
 }
 
 // returns a MakeCodeRequest object
@@ -183,11 +208,52 @@ func MakingDataRequest(tokenResponse *TokenResponse) {
 	}
 	req.Header.Set("Authorization", "Bearer "+tokenResponse.Access_token)
 	response, err := client.Do(req)
-	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Fatal("Error reading body")
+		log.Fatalf("Error making request %v", err)
 	}
-	fmt.Println(string(body))
-	//TODO: display the artists names of the musics (10 firsts)
-	// the name of the music and the date it was played
+	if response.StatusCode != 200 {
+		//TODO: deal if access token expired
+		log.Fatalf("Error %v", response)
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	var data Response
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		log.Fatalf("error parsing json %v", err)
+	}
+	fmt.Println(data)
+	itens := data.Items
+	var size int
+	if len(itens) < 10 {
+		size = len(itens)
+	} else {
+		size = 10
+	}
+	for i := 0; i < size; i++ {
+		item := itens[i]
+		dt, err := time.Parse("2006-01-02T15:04:05.000Z", item.Played_at)
+		loc, err := time.LoadLocation("America/Recife")
+		if err != nil {
+			log.Fatalf("Error loading location %v", err)
+		}
+		fmt.Printf("Music name: %s\n", item.Track.Name)
+		fmt.Printf("Album name: %s\n", item.Track.Album.Name)
+		if err == nil {
+			dtStr := dt.In(loc).Format(time.UnixDate)
+			fmt.Printf("Played at: %v\n", dtStr)
+		}
+		fmt.Println("Artits (just the first 2):")
+		artits := item.Track.Album.Artists
+		var artistsSize int
+		if len(artits) < 2 {
+			artistsSize = len(artits)
+		} else {
+			artistsSize = 2
+		}
+		for j := 0; j < artistsSize; j++ {
+			fmt.Printf("   %d- %v\n", j+1, artits[j].Name)
+		}
+		fmt.Println("==================")
+		fmt.Println()
+	}
 }
